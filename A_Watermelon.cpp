a4e314116bf2957706db2af63436316c5e558f7a
manas25527 <manas25527@gmail.com>
2026-08-13 00:10:23 +0530

    Add A. Watermelon problem solution

---
+++ A_Watermelon.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n == 2)
+    {
+        cout << "NO";
+    }
+    
+    else if (n%2 != 0)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+
+    return 0;
+}
